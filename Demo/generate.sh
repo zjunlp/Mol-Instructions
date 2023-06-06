e72dc69ce05364108aa9a9cb129132db456d4a99
HRHRHRHR666 <85422053+HRHRHRHR666@users.noreply.github.com>
2023-06-06 15:35:27 +0800

    Update generate.sh

--- Demo/generate.sh
+++ Demo/generate.sh
@@ -2,6 +2,7 @@ BASE_MODEL_PATH=" "
 FINETUNED_MODEL_PATH=" "
 
 CUDA_VISIBLE_DEVICES=0 python generate.py \
+    --interactive False\
     --protein False\
     --load_8bit \
     --base_model $BASE_MODEL_PATH \
